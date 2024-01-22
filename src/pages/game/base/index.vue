77b126be8a7fdedd7a83d982932cbd3909aed6f9
Kabuda-czh <634469564@qq.com>
2024-01-23 02:35:25 +0800

    ✨ feat: 增加游戏列表页

---
+++ src/pages/game/base/index.vue
@@ -0,0 +1,202 @@
+<template>
+  <div>
+    <t-card class="list-card-container" :bordered="false">
+      <t-table
+        :data="data"
+        :columns="COLUMNS"
+        :row-key="rowKey"
+        vertical-align="top"
+        :hover="true"
+        :pagination="pagination"
+        :loading="dataLoading"
+        :header-affixed-top="headerAffixedTop"
+        @page-change="rehandlePageChange"
+        @change="rehandleChange"
+      >
+        <template #icon="slotProps">
+          <t-avatar :src="slotProps.icon" size="default" />
+        </template>
+
+        <template #op="slotProps">
+          <t-space>
+            <t-link theme="primary" @click="handleClickDetail()"> {{ $t('pages.gameBase.detail') }}</t-link>
+            <t-link theme="primary" @click="handleClickDetail()"> {{ $t('pages.gameBase.start') }}</t-link>
+            <t-link theme="danger" @click="handleClickDelete(slotProps)"> {{ $t('pages.gameBase.stop') }}</t-link>
+          </t-space>
+        </template>
+        <template #more="slotProps">
+          <t-space>
+            <t-link theme="danger" @click="handleClickRefund(slotProps)"> {{ $t('pages.gameBase.refund') }}</t-link>
+          </t-space>
+        </template>
+      </t-table>
+    </t-card>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  name: 'GameBase',
+};
+</script>
+
+<script setup lang="ts">
+import { PrimaryTableCol, Progress, TableRowData } from 'tdesign-vue-next';
+import { computed, onMounted, ref } from 'vue';
+import { useRouter } from 'vue-router';
+
+import { getList } from '@/api/list';
+import { prefix } from '@/config/global';
+import { t } from '@/locales';
+import { useSettingStore } from '@/store';
+
+const store = useSettingStore();
+
+const progressColor = (percentage: number): string => {
+  if (percentage < 50) {
+    return '#13ce66';
+  }
+  if (percentage < 80) {
+    return '#e6a23c';
+  }
+  return '#ff4949';
+};
+
+const COLUMNS: PrimaryTableCol<TableRowData>[] = [
+  {
+    // @ts-ignore
+    title: t('pages.gameBase.gameIcon'),
+    colKey: 'icon',
+    width: 100,
+    align: 'center',
+  },
+  { title: t('pages.gameBase.gameName'), colKey: 'name', ellipsis: true, align: 'center' },
+  {
+    title: t('pages.gameBase.gameCpu'),
+    ellipsis: true,
+    colKey: 'cpu',
+    align: 'center',
+    cell: (h, { row }) => {
+      return h(Progress, { percentage: +row.cpu, color: progressColor(+row.cpu) });
+    },
+  },
+  {
+    title: t('pages.gameBase.gameMemory'),
+    ellipsis: true,
+    colKey: 'memory',
+    align: 'center',
+    cell: (h, { row }) => {
+      return h(Progress, { percentage: +row.memory, color: progressColor(+row.memory) });
+    },
+  },
+  {
+    title: t('pages.gameBase.operation'),
+    colKey: 'op',
+    align: 'center',
+  },
+  {
+    title: t('pages.gameBase.more'),
+    colKey: 'more',
+    width: 80,
+    align: 'center',
+  },
+];
+
+const data = ref([]);
+const pagination = ref({
+  defaultPageSize: 20,
+  total: 100,
+  defaultCurrent: 1,
+});
+
+const dataLoading = ref(false);
+const fetchData = async () => {
+  dataLoading.value = true;
+  try {
+    const { list } = await getList();
+    data.value = list;
+    pagination.value = {
+      ...pagination.value,
+      total: list.length,
+    };
+  } catch (e) {
+    console.log(e);
+  } finally {
+    dataLoading.value = false;
+  }
+};
+
+onMounted(() => {
+  fetchData();
+});
+
+const confirmVisible = ref(false);
+
+const router = useRouter();
+
+const rowKey = 'index';
+
+const rehandlePageChange = (curr: unknown, pageInfo: unknown) => {
+  console.log('分页变化', curr, pageInfo);
+};
+
+const rehandleChange = (changeParams: unknown, triggerAndData: unknown) => {
+  console.log('统一Change', changeParams, triggerAndData);
+};
+
+const handleClickDetail = () => {
+  router.push('/detail/base');
+};
+
+const handleClickDelete = (row: { rowIndex: any }) => {
+  confirmVisible.value = true;
+};
+
+const handleClickRefund = (row: { rowIndex: any }) => {
+  console.log('退款', row);
+};
+
+const headerAffixedTop = computed(
+  () =>
+    ({
+      offsetTop: store.isUseTabsRouter ? 48 : 0,
+      container: `.${prefix}-layout`,
+    }) as any,
+);
+</script>
+
+<style lang="less" scoped>
+.payment-col {
+  display: flex;
+
+  .trend-container {
+    display: flex;
+    align-items: center;
+    margin-left: var(--td-comp-margin-s);
+  }
+}
+
+.list-card-container {
+  padding: var(--td-comp-paddingTB-xxl) var(--td-comp-paddingLR-xxl);
+
+  :deep(.t-card__body) {
+    padding: 0;
+  }
+}
+
+.left-operation-container {
+  display: flex;
+  align-items: center;
+  margin-bottom: var(--td-comp-margin-xxl);
+
+  .selected-count {
+    display: inline-block;
+    margin-left: var(--td-comp-margin-l);
+    color: var(--td-text-color-secondary);
+  }
+}
+
+.search-input {
+  width: 360px;
+}
+</style>
